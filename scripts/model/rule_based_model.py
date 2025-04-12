c06e3fd7fcfd354d1b1211b69dcf2eb682eb9827
Jekaterina Jaroslavceva <jekatrinaj@microsoft.com>
2025-04-12 21:41:11 +0200

    model

--- scripts/model/rule_based_model.py
+++ scripts/model/rule_based_model.py
@@ -22,6 +22,10 @@ class SimpleModel(Model):
     def predict(self, client: ClientData) -> int:
         if flag_missing_values(client):
             return 0
+        if flag_verify_email(client):
+            return 0
+        if flag_phone(client):
+            return 0
         return 1
 
 
@@ -31,7 +35,6 @@ def flag_missing_values(client: ClientData):
     NULLABLE_FIELDS = ("middle_name", "other_ccy", "employer", "position", "annual_income", "previous_profession", "is_primary", "source_info", "account_number", "expected_transactional_behavior")
 
     for data in (client.client_profile, client.client_description, client.passport, client.account_form):
-        print(data)
         for path, value in data.items():
             if isinstance(path, str):
                 path = path.lower()
@@ -47,4 +50,30 @@ def flag_missing_values(client: ClientData):
                 return True
     return False
 
+
+def flag_verify_email(client: ClientData):
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    if client.account_form["email"] != client.client_profile["contact_info"]["email"]:
+        return True
+
+    if not re.match(email_pattern, client.account_form["email"]):
+        return True
+
+    return False
+
+
+def flag_phone(client: ClientData):
+    phone_number = client.account_form["phone_number"].replace(" ", "")
+
+    if phone_number != client.client_profile["contact_info"]["telephone"].replace(" ", ""):
+        return True
+
+    if not re.match("^\+?\d+$", phone_number):
+        return True
+
+    if len(phone_number) > 15 or len(phone_number) < 8:
+        return True
+
+    return False
 
