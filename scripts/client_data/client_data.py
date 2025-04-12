8e8f6484f2424c8d46024b17c205f5babfb8c74c
Jekaterina Jaroslavceva <jekatrinaj@microsoft.com>
2025-04-12 20:43:56 +0200

    up

--- scripts/client_data/client_data.py
+++ scripts/client_data/client_data.py
@@ -4,10 +4,6 @@ from dataclasses import dataclass
 PROJECT_DIR = Path(__file__).parent.parent.parent.resolve().absolute()
 
 data_dir = PROJECT_DIR / "data"
-<<<<<<< HEAD
-print(data_dir)
-=======
->>>>>>> d61c4347120d85ccaa848ca661b5c4386a65188d
 
 @dataclass
 class ClientData:
@@ -18,8 +14,4 @@ class ClientData:
     client_description: dict
     client_profile: dict
     passport: dict
-<<<<<<< HEAD
     label: int | None = None
-=======
-    label: int | None = None
->>>>>>> d61c4347120d85ccaa848ca661b5c4386a65188d
