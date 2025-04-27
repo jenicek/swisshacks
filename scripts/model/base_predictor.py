ca34f1a89ad5bd0d42f9875715c776517afa9f0e
Jekaterina Jaroslavceva <jekatrinaj@microsoft.com>
2025-04-27 23:12:43 +0200

    remove unused base predictor.py

--- scripts/model/base_predictor.py
+++
@@ -1,8 +0,0 @@
-class Predictor:
-    """Base predictor class"""
-
-    def predict(self, data, *args, **kwargs):
-        return self._predict(data, *args, **kwargs)
-
-    def _predict(self, data, *args, **kwargs):
-        raise NotImplementedError("Subclasses must implement this method")
